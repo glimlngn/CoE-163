9f46e9b2cb67c9481df11a5661456b79fa33449a
glimlngn <gflimlingan@up.edu.ph>
2024-04-06 17:38:03 +0800

    findMinCombi now executes "string" algorithm with proper RNG

--- se01/se01-limlingan.cpp
+++ se01/se01-limlingan.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <string>
-#include <algorithm>    // for std::max and std::min
-#include <cmath>    // for std::pow and std::abs
-#include <chrono>   // for runtime profiling
 #include <random>
+#include <chrono>   // for runtime profiling
 
-int findMinCombi(int number_of_locks, long int init_combi, long int unlock_combi){
-    int bigger_num, smaller_num, min_steps = 0;
-    while(number_of_locks > 0){   // loop goes through all digits of the lock
-        bigger_num = std::max(init_combi % 10, unlock_combi % 10);    // decides placement of values for the following formula
-        smaller_num = std::min(init_combi % 10, unlock_combi % 10);
-        min_steps += std::min(std::abs(bigger_num - smaller_num), std::abs((bigger_num - 10) - smaller_num));    // adds min. steps of digit, number wrapping is emulated
-        init_combi /= 10;     // iterates to the next digit (from right to left)
-        unlock_combi /= 10;
-        number_of_locks--;
+int findMinCombi(int number_of_locks, std::string init_combi, std::string unlock_combi){
+    int bigger_num, smaller_num, temp;
+    int min_steps = 0;;
+    while(number_of_locks > 0){
+        bigger_num = int(init_combi[number_of_locks - 1] - '0');
+        smaller_num = int(unlock_combi[number_of_locks - 1] - '0');
+        if(bigger_num < smaller_num){
+            temp = bigger_num;
+            bigger_num = smaller_num;
+            smaller_num = temp;
+        }
+        min_steps += std::min(bigger_num - smaller_num, (smaller_num + 10) - bigger_num);
+    number_of_locks--;
     }
-    return min_steps;    // returns minimum number of steps of the lock
+    return min_steps;
 }
 
- std::pair<double, double> runtimeProfiler(int number_of_locks, long int init_combi, long int unlock_combi){
-    // NOTE: Leading zeroes are considered in runtime profiling.
-    int number_of_locks_random;
-    uint64_t init_combi_random, unlock_combi_random;
+std::pair<double, double> runtimeProfiler(int number_of_locks, std::string init_combi, std::string unlock_combi){
     double runtime_same_length, runtime_diff_length = 0;
     
     // Case 1: Same Length
     // find_min_combi is iterated 1000 times with programmed input
+    
     auto start_time = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < 1000; i++)
         findMinCombi(number_of_locks, init_combi, unlock_combi);
@@ -34,25 +34,29 @@ int findMinCombi(int number_of_locks, long int init_combi, long int unlock_combi
     runtime_same_length = elapsed_time.count();
 
     // Case 2: Different Length
+    int number_of_locks_random;
+    std::string init_combi_random, unlock_combi_random;
+
     std::random_device rd;  // initialize random number generator
     std::mt19937_64 gen(rd()); 
-    std::uniform_int_distribution<long long> dis1(1, 100); // for random number of locks N: [1, 100)
+    std::uniform_int_distribution<int> dis1(1, 100); // for random number of locks N: [1, 100]
+    std::uniform_int_distribution<int> dis2(0, 9); // for random digits: [0, 9]
 
     for(int i = 0; i < 1000; i++){
-        number_of_locks_random = dis1(gen) + 1;
-        std::uniform_int_distribution<long long> dis2(0, std::llround(std::pow(10, number_of_locks_random))); // for random lock configurations
-        init_combi_random = dis2(gen);
-        unlock_combi_random = dis2(gen);
-        
-        start_time = std::chrono::high_resolution_clock::now();
-        
-        findMinCombi(number_of_locks_random, init_combi_random, unlock_combi_random);
-        // std::cout << i << ": " << number_of_locks_random << " " << init_combi_random << " " << unlock_combi_random << " " << findMinCombi(number_of_locks_random, init_combi_random, unlock_combi_random) << std::endl;
-        
-        end_time = std::chrono::high_resolution_clock::now();
+        number_of_locks_random = dis1(gen);
+        init_combi_random = "", unlock_combi_random = "";
+        for(int j = 0; j < number_of_locks_random; j++){
+            init_combi_random += std::to_string(dis2(gen));
+            unlock_combi_random += std::to_string(dis2(gen));
+        }
 
+        start_time = std::chrono::high_resolution_clock::now();
+        findMinCombi(number_of_locks_random, init_combi_random, unlock_combi_random);
+        // std::cout << i + 1 << ": " << number_of_locks_random << " " << init_combi_random << " " << unlock_combi_random << " " << findMinCombi(number_of_locks_random, init_combi_random, unlock_combi_random) << std::endl;
+        end_time = std::chrono::high_resolution_clock::now();
         elapsed_time = end_time - start_time;
         runtime_diff_length += elapsed_time.count();
+
         }
         return std::make_pair(runtime_same_length, runtime_diff_length);  // returns runtimes
     }
@@ -71,11 +75,8 @@ int main(){
         return 1;
     }
 
-    long int init_combi_long_int = std::stol(init_combi);
-    long int unlock_combi_long_int = std::stol(unlock_combi);
-
-    min_combi = findMinCombi(number_of_locks, init_combi_long_int, unlock_combi_long_int);   // output
-    std::pair<double, double> runtimes = runtimeProfiler(number_of_locks, init_combi_long_int, unlock_combi_long_int);
+    min_combi = findMinCombi(number_of_locks, init_combi, unlock_combi);   // output
+    std::pair<double, double> runtimes = runtimeProfiler(number_of_locks, init_combi, unlock_combi);
     runtime_same_length = runtimes.first;
     runtime_different_length = runtimes.second;
 
